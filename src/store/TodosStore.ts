4e06c2924a69d6eb1ae87d63400ff902a39a85d7
Artur <apkartas225@yandex.ru>
2023-02-08 11:20:55 +0300

    fix checked item

--- src/store/TodosStore.ts
+++ src/store/TodosStore.ts
@@ -9,18 +9,14 @@ class TodoStore {
         makeAutoObservable(this)
     }
     add(todo: ITodo) {
-        console.log('add')
         this.todos.push(todo)
     }
     remove(id: number) {
-        console.log('remove')
         this.todos = this.todos.filter((el: ITodo) => el.id !== id)
         console.log('remove.todos', this.todos)
     }
 
     selectTodo( id: number, checked: boolean ) {
-        console.log('id, checked', id, checked)
-        console.log('selectTodo')
         this.todos = this.todos.map( (el: ITodo) => {
             if (el.id === id) {
                 el.checked = checked;
@@ -28,7 +24,6 @@ class TodoStore {
             }
             return el;
         })
-        console.log('this.todos', this.todos)
     }
 }
 
